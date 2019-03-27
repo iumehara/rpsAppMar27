3f495d8fc7d084b9174b322e68198e695ec3ef60
murakamij01 <>
2019-03-27 17:53:09 +0900

    modify to unificate Ubiquitous Language

--- rps/src/rps.js
+++ rps/src/rps.js
@@ -1,23 +1,23 @@
 class Requests {
-  play(throw1, throw2, observer) {
+  play(p1Hand, p2Hand, observer) {
 
     let isValidThrow = (targetThrow) => {
       return targetThrow === 'rock' || targetThrow === 'scissors' || targetThrow === 'paper'
     }
 
-    if (!isValidThrow(throw1) || !isValidThrow(throw2)) {
+    if (!isValidThrow(p1Hand) || !isValidThrow(p2Hand)) {
       observer.noGame()
       return
     }
 
-    if (throw1 === throw2) {
+    if (p1Hand === p2Hand) {
       observer.draw()
       return
     }
 
-    if ((throw1 === 'rock' && throw2 === 'scissors') ||
-        (throw1 === 'paper' && throw2 === 'rock') ||
-        (throw1 === 'scissors' && throw2 === 'paper')) {
+    if ((p1Hand === 'rock' && p2Hand === 'scissors') ||
+        (p1Hand === 'paper' && p2Hand === 'rock') ||
+        (p1Hand === 'scissors' && p2Hand === 'paper')) {
       observer.p1Wins()
     } else {
       observer.p2Wins()
