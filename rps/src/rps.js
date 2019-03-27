d3e8198d0932cc6ba1f674ad1f3a836113a68510
iumehara <iumehara10@gmail.com>
2019-03-27 14:41:25 +0900

    remove semicolons

--- rps/src/rps.js
+++ rps/src/rps.js
@@ -1,19 +1,19 @@
 class Requests {
     play(p1hand, p2hand, observer) {
-        const validHands = ['scissors', 'rock', 'paper'];
+        const validHands = ['scissors', 'rock', 'paper']
         if (!validHands.includes(p1hand) ||
             !validHands.includes(p2hand)) {
-            observer.noGame();
+            observer.noGame()
         }  else if (p1hand === p2hand) {
-            observer.draw();
+            observer.draw()
         } else if (p1hand === 'scissors' && p2hand === 'rock' ||
             p1hand === 'paper' && p2hand === 'scissors' ||
             p1hand === 'rock' && p2hand === 'paper') {
-            observer.p2Wins();
+            observer.p2Wins()
         } else {
-            observer.p1Wins();
+            observer.p1Wins()
         }
     }
 }
 
-module.exports = {Requests};
+module.exports = {Requests}
