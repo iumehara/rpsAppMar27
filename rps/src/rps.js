49859e305603f821a4faa23e1a7635a1c533fd2b
Kaylee Mann <kayleemann7@gmail.com>
2019-03-28 09:35:38 +0900

    day 2 start

--- rps/src/rps.js
+++ rps/src/rps.js
@@ -1,14 +1,20 @@
 class Requests {
+    player2Wins(p1hand, p2hand) {
+        return p1hand === 'scissors' && p2hand === 'rock' ||
+            p1hand === 'paper' && p2hand === 'scissors' ||
+            p1hand === 'rock' && p2hand === 'paper';
+    }
+    invalid(p1hand, p2hand) {
+        const validHands = ['scissors', 'rock', 'paper'];
+        return !validHands.includes(p1hand) ||
+            !validHands.includes(p2hand);
+    }
     play(p1hand, p2hand, observer) {
-        const valid_hands = ['scissors', 'rock', 'paper'];
-        if (!valid_hands.includes(p1hand) ||
-            !valid_hands.includes(p2hand)) {
+        if (this.invalid(p1hand, p2hand)) {
             observer.invalid();
         }  else if (p1hand === p2hand) {
             observer.draw();
-        } else if (p1hand === 'scissors' && p2hand === 'rock' ||
-            p1hand === 'paper' && p2hand === 'scissors' ||
-            p1hand === 'rock' && p2hand === 'paper') {
+        } else if (this.player2Wins(p1hand, p2hand)) {
             observer.p2Wins();
         } else {
             observer.p1Wins();
