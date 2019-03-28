e4080d11505d3480334a1ceeb840983841c6fe37
Kaylee Mann <kayleemann7@gmail.com>
2019-03-28 10:36:10 +0900

    Add web title test

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,26 @@
+import ReactDOM from "react-dom"
+import React from 'react'
+import PlayForm from "../src/PlayForm";
+
 describe('RPS App', function () {
-  it('wiring test', function () {
-    expect(true).toBe(true)
+  let domFixture
+
+  beforeEach(() => {
+    domFixture = document.createElement('div')
+    document.querySelector('body')
+        .appendChild(domFixture)
+  })
+
+  afterEach(() => {
+    domFixture.remove()
+  })
+
+  it('displays title', () => {
+    ReactDOM.render(
+        <PlayForm/>,
+        domFixture
+    )
+
+    expect(domFixture.innerText).toContain('Play Game')
   })
 })
---
+++ web/src/PlayForm.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default class PlayForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>Play Game</div>
+            </div>
+        )
+    }
+}
