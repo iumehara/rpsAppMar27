6ec2fbb3be4e2381919981d648ec0ee686608150
Kaylee Mann <kayleemann7@gmail.com>
2019-03-28 11:45:31 +0900

    Add stub tests for PlayForm

--- web/src/PlayForm.js
+++ web/src/PlayForm.js
@@ -1,10 +1,37 @@
 import React from "react"
 
 export default class PlayForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    playButtonClicked() {
+        this.props.requests.playRound('', '', this)
+    }
+
+    p1Wins() {
+        this.setState({result: 'Player 1 Wins!'})
+    }
+
+    p2Wins() {
+        this.setState({result: 'Player 2 Wins!'})
+    }
+
+    draw() {
+        this.setState({result: 'Draw!'})
+    }
+
+    noGame() {
+        this.setState({result: 'No Game!'})
+    }
+
     render() {
         return (
             <div>
                 <div>Play Game</div>
+                <button onClick={this.playButtonClicked.bind(this)}>PLAY!</button>
+                <div>{this.state.result}</div>
             </div>
         )
     }
